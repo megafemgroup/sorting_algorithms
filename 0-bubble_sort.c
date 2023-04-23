4e589b7ce9938140a27478c8e75e748e83877116
Mehalayese Femi <megafemgroup@gmail.com>
2023-04-23 04:20:15 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
+ *
+ *@array: array of ints
+ *@size: size of array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp, i, j;
+	if (!array || !size)
+		return;
+	for (i = 0; i < size - 1; i++)
+       	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+
+		}
+	}
+}
