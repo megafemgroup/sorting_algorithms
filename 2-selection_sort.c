064db60aa6ab099785c14f648a145e6f3c3c863d
Mehalayese Femi <megafemgroup@gmail.com>
2023-04-24 13:09:13 -0700

    select sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ *selection_sort- function that sorts using selection sort algorithm
+ *
+ *@array: array to be sorted
+ *@size: size of the array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+}
